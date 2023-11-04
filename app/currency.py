deb736fb6cb20efa6a8fcb238ce82897a9e73c13
susu <yatiti84@gmail.com>
2023-11-04 19:30:57 +0800

    test: add test for currency convert

--- app/currency.py
+++ app/currency.py
@@ -3,10 +3,11 @@ from flask import current_app
 
 def parameter_check(params, currency_data):
 
-    source = params.get('source', default="", type=str)
-    target = params.get('target', default="", type=str)
-    amount = params.get('amount', default="0", type=str)
-    amount = int(amount.replace('$', '').replace(',', ''))
+    source = params.get('source', default='', type=str).upper()
+    target = params.get('target', default='', type=str).upper()
+    amount = params.get('amount', default='0', type=str)
+    amount = float(amount.replace('$', '').replace(',', ''))
+
     if source in currency_data['currencies'] and target in currency_data['currencies'][source]:
         return source, target, amount
     return False
@@ -15,24 +16,30 @@ def parameter_check(params, currency_data):
 def get_currency_data():
     if current_app.config['CURRENCY_MODE'] is False:
         return current_app.config['DEFAULT_CURRENCY']
-    # can add more sources to get the currency data
+    # TODO: add different sources to get the currency data
 
 
 def convert(params):
     currency_data = get_currency_data()
     parameters = parameter_check(params, currency_data)
-    if parameters:
+    if isinstance(parameters, tuple):
         source, target, amount = parameters
+        if amount < 0:
+            return {'msg': 'fail',
+                    'amount': f'${amount}',
+                    'error_msg': 'amount needs to be greater or equal than 0'
+                    }
         currency = currency_data['currencies'][source][target]
         converted = round(amount * currency, 2)
-        return {"msg": "success",
-                "amount": "${:,}".format(converted)
+        # TODO: check range of input amount correspond to currency
+        return {'msg': 'success',
+                'amount': '${:,}'.format(converted)
                 }
 
-    else:
-        return {"msg": "fail",
-                "amount": "$0"
-                }
+    return {'msg': 'fail',
+            'amount': '$0',
+            'error_msg': 'source or target not exists'
+            }
 
 
 # if __name__ == '__main__':
---
+++ tests/test_currency.py
@@ -0,0 +1,71 @@
+import unittest
+from flask_testing import TestCase
+from app import create_app
+
+
+class SettingBase(TestCase):
+    def create_app(self):
+        return create_app('testing')
+
+    def setUp(self):
+        self.source = 'USD'
+        self.target = 'JPY'
+        self.amount = '$1,525'
+
+class CheckCurrencyConvert(SettingBase):
+    def test_convert_currency(self):
+        response = self.client.get(
+            f'/convert?source={self.source}&target={self.target}&amount={self.amount}',)
+        data = response.get_json()
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['msg'], 'success')
+        self.assertEqual(data['amount'], '$170496.53')
+
+    def test_amount_float(self):
+        self.amount = '$1.525'
+        response = self.client.get(
+            f'/convert?source={self.source}&target={self.target}&amount={self.amount}',)
+        data = response.get_json()
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['msg'], 'success')
+        self.assertEqual(data['amount'], '$170.5')
+
+    def test_not_exist_currency_name(self):
+        self.source = 'ABC'
+        response = self.client.get(
+            f'/convert?source={self.source}&target={self.target}&amount={self.amount}',)
+        data = response.get_json()
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['msg'], 'fail')
+        self.assertEqual(data['errorMsg'], 'source or target not exists')
+
+    def test_missing_parameter(self):
+        response = self.client.get(
+            f'/convert?target={self.target}&amount={self.amount}',)
+        data = response.get_json()
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['msg'], 'fail')
+        self.assertEqual(data['errorMsg'], 'source or target not exists')
+
+    def test_case_sensitive(self):
+        self.target = 'jpy'
+        response = self.client.get(
+            f'/convert?source={self.source}&target={self.target}&amount={self.amount}',)
+        data = response.get_json()
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['msg'], 'success')
+        self.assertEqual(data['amount'], '$170496.53')
+
+    def test_negative_amount(self):
+        self.amount = '-1'
+        response = self.client.get(
+            f'/convert?source={self.source}&target={self.target}&amount={self.amount}',)
+        data = response.get_json()
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['msg'], 'fail')
+        self.assertEqual(data['error_msg'],
+                         'amount needs to be greater or equal than 0')
+    
+
+if __name__ == '__main__':
+    unittest.main()
